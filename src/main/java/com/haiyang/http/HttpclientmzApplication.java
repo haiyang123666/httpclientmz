96bf4ea6b3a520dfd35ef9796ccf14bb255a51ce
haiyang123666 <731747067@qq.com>
2019-07-10 14:38:51 +0800

    创建并提交项目

---
+++ src/main/java/com/haiyang/http/HttpclientmzApplication.java
@@ -0,0 +1,14 @@
+package com.haiyang.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpclientmzApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HttpclientmzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haiyang/http/HttpclientmzApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haiyang.http;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpclientmzApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
